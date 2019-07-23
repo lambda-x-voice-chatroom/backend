c5f0a136bfd536b2edbe844eecc6f26e673cffd1
Mike Landers <landers.mike@gmail.com>
2019-07-23 08:58:51 -0700

    removed getUserById

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -17,13 +17,13 @@ router.post('/', async (req, res) => {
             .catch(function(error) {
                 res.status(403).json({ message: 'Invalid token', data: error });
             });
-        const user = await getUserById(id);
-        if (user) {
-            res.send(200).json({ message: 'Succcess', data: user });
-        } else {
-            const user = addUser(req.body.user);
-            res.send(201).json({ message: 'User Created', data: user });
-        }
+        // const user = await getUserById(id);
+        // if (user) {
+        //     res.send(200).json({ message: 'Succcess', data: user });
+        // } else {
+        const user = addUser(req.body.user);
+        res.send(201).json({ message: 'User Created', data: user });
+        // }
         // validate user is in the system
         // Get initial user data from DB and send back to client
     } catch (error) {
