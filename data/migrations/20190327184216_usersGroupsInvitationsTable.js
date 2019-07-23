79ce2d07b0d5d5cdaff01240f592414a962d9987
Mike Landers <landers.mike@gmail.com>
2019-07-23 15:04:20 -0700

    updates migration files

--- data/migrations/20190327184216_usersGroupsInvitationsTable.js
+++ data/migrations/20190327184216_usersGroupsInvitationsTable.js
@@ -1,12 +1,18 @@
-exports.up = function (knex, Promise) {
-    return knex.schema.createTable('usersGroupsInvitations', function (tbl) {
-        tbl.integer('userId').references('id').inTable('users').onDelete('CASCADE');
-        tbl.integer('groupId').references('id').inTable('groups').onDelete('CASCADE');
-        tbl.timestamp('createdAt').defaultTo(knex.fn.now())
-    })
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('usersGroupsInvitations', function(tbl) {
+        tbl.string('userId')
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE');
+        tbl.integer('groupId')
+            .references('id')
+            .inTable('groups')
+            .onDelete('CASCADE');
+        tbl.timestamp('createdAt').defaultTo(knex.fn.now());
+    });
 };
 
-exports.down = function (knex, Promise) {
+exports.down = function(knex, Promise) {
     // drop the usersGroupsInvitations table
     return knex.schema.dropTableIfExists('usersGroupsInvitations');
 };
