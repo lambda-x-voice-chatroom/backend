6468393d7bfef1b93409c568bd5341b769e82a27
Mike Landers <landers.mike@gmail.com>
2019-07-24 14:10:38 -0700

    cleaned up code

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -6,29 +6,26 @@ const { getUserById, addUser } = require('../users/usersModel');
 router.get('/', async (req, res) => {
     const token = req.headers.authorization;
     try {
-        // Get UID from Google
         let decodedToken = await admin.auth().verifyIdToken(token);
         if (decodedToken) {
             let firebaseUser = await admin.auth().getUser(decodedToken.uid);
             let user = await getUserById(firebaseUser.uid);
             if (user != -1) {
-                console.log('if');
                 res.status(200).json({
                     message: 'success',
-                    data: { user }
+                    data: user
                 });
             } else {
-                console.log('else');
-                const userInfo = await addUser({
+                const user = await addUser({
                     id: firebaseUser.uid,
                     displayName: firebaseUser.displayName,
                     email: firebaseUser.email,
                     avatar: firebaseUser.photoURL
                 });
-                if (userInfo) {
+                if (user != -1) {
                     res.status(201).json({
                         message: 'User Created',
-                        data: { userInfo }
+                        data: user
                     });
                 } else {
                     res.status(500).json({ message: 'Unable to add user' });
@@ -38,7 +35,8 @@ router.get('/', async (req, res) => {
             res.status(403).json({ message: 'Invalid token', data: error });
         }
     } catch (error) {
-        console.log(error);
+        console.log('GET /Users : ', error);
+        res.status(500).json({ message: 'Server error', data: error });
     }
 });
 
